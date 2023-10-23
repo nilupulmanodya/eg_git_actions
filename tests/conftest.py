1ddeeb85e172feeb477006cafcf3765af87b4cfd
unknown <n.manodya@gmail.com>
2023-10-23 10:38:09 +0530

    testcases implementation with flsk

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from main import APP
+
+@pytest.fixture()
+def app():
+    app = APP
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
